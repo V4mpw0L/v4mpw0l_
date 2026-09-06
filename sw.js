d5387c0abe2c8b34a1f9b593fc2387a7e144ad3e
V4mpw0l <125161567+V4mpw0L@users.noreply.github.com>
2026-09-06 06:09:47 -0700

    att

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // V4MPW0L // TIAGO CARDOSO — Stealth PWA Core Service Worker
-const APP_VERSION = '2.1.1';
+const APP_VERSION = '2.1.2';
 const CACHE_NAME = `v4mpw0l-core-v${APP_VERSION}`;
 
 const PRECACHE_ASSETS = [
